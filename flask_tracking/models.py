6fd19fa8abff9976cb95462be96627c866cdf7f6
Raven Robertson <ravewillow6383@gmail.com>
2019-10-04 11:20:55 -0700

    building up models

---
+++ flask_tracking/models.py
@@ -0,0 +1,16 @@
+from flask.ext.sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Site(db.Model):
+    __tablename__ = 'tracking_site'
+
+    id = db.Column(db.Integer, primary_key=True)
+    base_url = db.Column(db.String)
+    visits = db.relationship('Visit', backref='tracking_site', lazy='select')
+
+    def __repr__(self):
+        return '<Site {:d} {}>'.format(self.id, self.base_url)
+
+    def __str__(self):
+        
